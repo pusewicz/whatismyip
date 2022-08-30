8ef39f8811b42fbb7829378d9325420c6a1ab62c
Piotr Usewicz <piotr@layer22.com>
2022-08-30 11:23:31 +0200

    Add etags

--- lib/whatismyip.rb
+++ lib/whatismyip.rb
@@ -6,24 +6,29 @@ require_relative "svg_status"
 class WhatIsMyIP < Roda
   VERSION = "1.1.0"
   CSS = File.read('assets/style.min.css')
+  INDEX_ETAG = Digest::SHA1.hexdigest(File.read('views/index.slim'))
 
   plugin :default_headers, {
     'Application' => self.class.name,
     'Version' => VERSION,
     'Cache-Control' => 'public, max-age=604800'
   }
+  plugin :caching
   plugin :public
   plugin :render, engine: 'slim'
   plugin :type_routing, types: { yaml: 'application/x-yaml', txt: 'text/plain', svg: 'image/svg+xml' }
 
   route do |r|
     r.root do
+      r.etag INDEX_ETAG
       view('index')
     end
 
     r.get(/ip(\.svg|\.txt|\.json|\.yaml|\.xml)?/) do
       response.headers['Cache-Control'] = 'private, max-age=604800, must-revalidate'
       @extracted_ip = extract_remote_ip(r)
+
+      r.etag @extracted_ip
 
       r.json { %("#{@extracted_ip}") }
       r.svg { SVGStatus.new(@extracted_ip).to_svg }
