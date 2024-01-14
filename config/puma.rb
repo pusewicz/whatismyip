fa3f1869aa5c0772caca934819d42c971d0d2719
Piotr Usewicz <piotr@layer22.com>
2024-01-14 18:16:22 +0100

    Use new defaults

--- config/puma.rb
+++ config/puma.rb
@@ -1,2 +1,16 @@
 port ENV.fetch('PORT', 3000)
-environment ENV.fetch('RACK_ENV', 'development')
+
+rails_env = ENV.fetch("RAILS_ENV", "development")
+environment rails_env
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
+threads threads_count, threads_count
+
+if rails_env == "production"
+  processors_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
+  if processors_count > 1
+    workers worker_count
+  else
+    preload_app!
+  end
+end
